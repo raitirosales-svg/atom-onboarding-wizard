958558c22ebfc879d5d1deb95543dd818c5a09bf
raitirosales-svg <raiti.rosales@atomchat.io>
2026-07-29 13:55:24 -0600

    FIX: Add missing ReactFlowProvider - canvas was crashing because useNodesState requires it in @xyflow/react v12

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import { ReactFlowProvider } from '@xyflow/react';
 import App from './App.tsx';
 import './index.css';
 import { LanguageProvider } from './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
+    <ReactFlowProvider>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </ReactFlowProvider>
   </StrictMode>,
 );
